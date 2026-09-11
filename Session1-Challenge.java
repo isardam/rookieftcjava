dec83c0bbc602ea12b58517799317da13d0387aa
One Madrasi <gayathri.swa@gmail.com>
2026-09-11 14:15:21 -0500

    Create Session1-Challenge.java

---
+++ Session1-Challenge.java
@@ -0,0 +1,20 @@
+/* "Your robot refuses to start. Find the problems and get Mission Control running." */
+
+public class MissionControl {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== ROBOT MISSION CONTROL ===")
+        System.out.println("Robot: Iron Angel");
+        System.out.println("Status: READY");
+
+        System.out.println("MISSION START")
+
+        System.out.println("1. Wake up robot");
+        System.out.println("2. Check battery");
+        System.out.println("3. Move forward")
+        System.out.println("4. Stop");
+
+        System.out.println("MISSION COMPLETE!");
+    }
+}
